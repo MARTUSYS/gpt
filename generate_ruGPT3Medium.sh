c455b8e973840e2936063d07c27b948d6d29155b
MARTUSYS <MARTUSYS@users.noreply.github.com>
2021-01-21 19:09:15 +1000

    0.014v

--- generate_ruGPT3Medium.sh
+++ generate_ruGPT3Medium.sh
@@ -2,8 +2,8 @@ python generate_transformers.py \
     --model_name_or_path=sberbank-ai/rugpt3medium_based_on_gpt2 \
     --path_to_prompt=sample/1.txt \
     --path_to_save_sample=sample/sample.txt \
-    --length=128 \
-    --temperature=0.9 \
-    --k=50 \
-    --p=0.95 \
-    --num_return_sequences 3 \
+    --length 128 \
+    --temperature 0.9 \
+    --k 50 \
+    --p 0.95 \
+    --num_return_sequences 3
--- generate_transformers.py
+++ generate_transformers.py
@@ -55,10 +55,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name_or_path", default=None, type=str, required=True, help="Path to pre-trained model or shortcut name selected in the list")
     parser.add_argument("--path_to_prompt", default="", type=str)
-    parser.add_argument("--path_to_save_sample", default='', type=str, help="Path to save sample")
-
+    parser.add_argument("--path_to_save_sample", action="store_true", default='', type=str, help="Path to save sample")
     parser.add_argument("--length", type=int, default=128)
-
     parser.add_argument("--temperature", type=float, default=1.0, help="temperature of 1.0 has no effect, lower tend toward greedy sampling")
     parser.add_argument("--repetition_penalty", type=float, default=1.0, help="primarily useful for CTRL model; in that case, use 1.2")
     parser.add_argument("--k", type=int, default=50)
